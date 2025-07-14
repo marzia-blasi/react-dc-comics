5395f3e9f864d68e97a5e12cf31a53e1a8d8296d
marzia <you@example.com>
2025-07-14 22:48:23 +0200

    fix App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+//import Header from './src/components/Header.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <h1> ciao</h1> 
     </>
   )
 }
 
 export default App
+
