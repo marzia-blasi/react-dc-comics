73a26b277b219179595177b2160fe0a33c3565ec
marzia <you@example.com>
2025-07-15 15:22:20 +0200

    fix container css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,20 +9,23 @@ export default function Main(){
         <main>
             <div className="container-fluid">
                 <div className="jumbo"><img src="./src/assets/img/jumbotron.jpg" alt="" /></div>
-                <div className="row d-flex">
-                    
-                    {comics.map(({ id, title, thumb }) => {
-                        return <div className="comics col-12 col-sm-6 col-lg-3" key={id}>
-                            
-                            <div><img src={thumb} alt={title} /></div>
-                            <div className='title'>{title}</div>
-                        </div>
-
-                    }
+                <div className="container">
+                    <div className="row d-flex">
                         
-                    )}
+                        {comics.map(({ id, title, thumb }) => {
+                            return <div className="comics col-12 col-sm-6 col-lg-3" key={id}>
+                                <div className="cardm">
+                                    <div><img src={thumb} alt={title} /></div>
+                                    <div className='title'>{title}</div>
+                                </div>    
+                            </div>
 
-                </div>
+                        })}
+                            
+                        
+
+                    </div>
+                </div>    
             </div>
         </main> 
     )
