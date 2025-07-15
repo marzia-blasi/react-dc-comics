b34d7cbc5b97a05dc1c21401a5abbad48d4fbf61
marzia <you@example.com>
2025-07-15 13:05:33 +0200

    modified main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 
-// export default comics 
+import comics from "./comics"
 
 
 export default function Main(){
@@ -9,7 +9,20 @@ export default function Main(){
         <main>
             <div className="container-fluid">
                 <div className="jumbo"><img src="./src/assets/img/jumbotron.jpg" alt="" /></div>
+                <div className="row d-flex">
+                    
+                    {comics.map(({ id, title, thumb }) => {
+                        return <div className="comics col-12 col-sm-6 col-lg-3" key={id}>
+                            
+                            <div><img src={thumb} alt={title} /></div>
+                            <div className='title'>{title}</div>
+                        </div>
 
+                    }
+                        
+                    )}
+
+                </div>
             </div>
         </main> 
     )
