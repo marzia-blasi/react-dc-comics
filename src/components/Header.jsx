3a2bc8ffe7141382b8d6dfaa8fbb6b5332c9e15b
marzia <you@example.com>
2025-07-15 17:40:39 +0200

    add db and fix jumbotron background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
-import navbar from "./navbar"
+import navbar from "./db/navbar";
 
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          <img src="./src/assets/img/dc-logo.png" alt="" />
+        </a>
 
-export default function Header(){
-
-    return (
-
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container">
-    <a className="navbar-brand" href="#"><img src="./src/assets/img/dc-logo.png" alt="" /></a>
-    
-    <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {navbar.map(({ id, title}) => {
-             return <li className="nav-item" key={id}>
-               <a className="nav-link active" aria-current="page" href="#">{title}</a>
-              </li>
-           })}
-      </ul>
-      
-    </div>
-  </div>
-</nav>
-    )
-
-
+        <div className="collapse navbar-collapse" id="navbarText">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            {navbar.map(({ id, title }) => {
+              return (
+                <li className="nav-item" key={id}>
+                  <a className="nav-link active" aria-current="page" href="#">
+                    {title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,34 +1,41 @@
+import comics from "./db/comics";
 
-import comics from "./comics"
-
-
-export default function Main(){
-
-    return (
-
-        <main>
-            <div className="container-fluid">
-                <div className="jumbo"><button class="btn btn-primary rounded-0">Bottone Spigolo Vivo</button></div>
-                <div className="container">
-                    <div className="row d-flex">
-                        
-                        {comics.map(({ id, title, thumb }) => {
-                            return <div className="comics col-12 col-sm-6 col-lg-3" key={id}>
-                                <div className="cardm">
-                                    <div><img src={thumb} alt={title} /></div>
-                                    <div className='title'>{title}</div>
-                                </div>    
-                            </div>
-
-                        })}
-                            
-                        
-
+export default function Main() {
+  return (
+    <main>
+      <div className="container-fluid">
+        <div className="jumbo">
+          <button class="btn btn-primary rounded-0">
+            Bottone Spigolo Vivo
+          </button>
+        </div>
+        <div className="container">
+          <div className="row d-flex">
+            {comics.map(({ id, title, thumb }) => {
+              return (
+                <div className="comics col-12 col-sm-6 col-lg-3" key={id}>
+                  <div className="cardm">
+                    <div>
+                      <img src={thumb} alt={title} />
                     </div>
-                </div>  
-                <div className="load d-flex"><button class="btn btn-primary rounded-0 ">Load More</button>
-</div>
-            </div>
-        </main> 
-    )
+                    <div className="title">{title}</div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="load d-flex">
+          <button class="btn btn-primary rounded-0 ">Load More</button>
+        </div>
+      </div>
+    </main>
+  );
 }
+
+/*
+<div>
+     <img src="./src/assets/img/jumbotron.jpg" alt="" />
+</div>
+
+*/
